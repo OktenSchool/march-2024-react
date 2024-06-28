90a1f5a904ed0e484d1cf7634e78a1b23e835e96
GrayHead <ss.zhuravlov@gmail.com>
2024-06-28 19:43:39 +0300

    cons 2 [feature] : pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
 
+    const [skipValue, setSkipValue] = useState<number>(0)
+
+    const [todos, setTodos] = useState<any[]>([]);
+    useEffect(() => {
+        fetch('https://dummyjson.com/todos?skip=' + skipValue)
+            .then(value => value.json())
+            .then(todosResponse => {
+                setTodos(todosResponse.todos);
+            });
+
+    }, [skipValue]);
+
 
     return (
-        <div></div>
+        <div>
+            {todos.map(value => <li>{value.todo}</li>)}
+
+            <hr/>
+            <button onClick={() => {
+                setSkipValue(skipValue - 30);
+                console.log('dec');
+            }}>prev
+            </button>
+            <button onClick={() => {
+
+                setSkipValue(skipValue + 30);
+                console.log('inc');
+            }}>next
+            </button>
+        </div>
     );
 }
 
