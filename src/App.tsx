daa040ff19374e802514082270f73d0358b0bc2d
GrayHead <ss.zhuravlov@gmail.com>
2024-07-01 19:29:47 +0300

    lesson 3 [update]:useState [update]:useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {ReactNode, useEffect, useRef, useState} from 'react';
 import './App.css';
 
 const App = () => {
 
+    // variable - reinit after each rerender
+    // useRef - store tate between rerender, but cannot render themself
+    // useState - useRef + rerender
+
+    const [counter, setCounter] = useState<number>(0);
+
+    const ref = useRef<any>();
+    useEffect(() => {
+        console.log(ref);
+    }, []);
+
 
     return (
-        <div></div>
+        <div>
+            <button
+                onClick={() => {
+                    setCounter(prevState => prevState + 1);
+
+                }}
+            >click me {counter}
+            </button>
+            <button ref={ref}
+                    onClick={() => {
+
+                        console.log(ref.current);
+
+                    }}
+            >ref {ref.current}
+            </button>
+            <h2>ref : {ref.current}</h2>
+        </div>
     );
 }
 
